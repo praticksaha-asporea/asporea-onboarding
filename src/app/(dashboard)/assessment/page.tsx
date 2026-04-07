71654d803d51ce4a86c6613cd3e4a296d520836c
anil6200 <anilkumar074042@gmail.com>
2026-04-07 18:07:07 +0530

    Implement Assessment Page

---
+++ src/app/(dashboard)/assessment/page.tsx
@@ -0,0 +1,8 @@
+import dynamic from 'next/dynamic'
+
+ 
+const Assessment = dynamic(() => import('@/Module/Candidate_Dashboard/Assessment'));
+
+export default function AssessmentPage() {
+  return <Assessment />
+}
