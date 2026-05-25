e73ccff74e45157184e514265437a0cab2bc02fc
Pratick <pratick.asporea@gmail.com>
2026-05-25 12:46:09 +0530

    remove .log

--- src/views/account-settings/account/AccountDetails.tsx
+++ src/views/account-settings/account/AccountDetails.tsx
@@ -131,7 +131,7 @@ const AccountDetails = () => {
         // console.log(payload,5844);
         
         const res = await axiosClient.patch(`/user/profile-update`, payload);
-        console.log(res,11111);
+        // console.log(res,11111);
 
         if (res.data?.success) {
           toast.success("Profile Updated Successfully!", {
